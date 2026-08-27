37510bd99882779a2dd822c572433369d1a84050
bitch <169540526+domeDiff@users.noreply.github.com>
2026-08-27 20:03:41 +0530

    Game-Over_panel

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -1,12 +1,41 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Score")]
     [SerializeField] private TMP_Text scoreText;
 
+    [Header("Game Over")]
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TMP_Text finalScoreText;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
     private void Update()
     {
+        if (GameManager.Instance.isGameOver)
+        {
+            ShowGameOver();
+            return;
+        }
+
         scoreText.text = "SCORE: " + scoreManager.Instance.score.ToString("D4");
+    }
+
+    private void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+        finalScoreText.text = "SCORE: " + scoreManager.Instance.score.ToString("D4");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
